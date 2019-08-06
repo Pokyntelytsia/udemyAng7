04fe4ae7ca8839d2151c77daf59be196dbea48f2
pokyntelytsia <dima@quarterback.gg>
2019-08-06 19:00:22 +0300

    fire auth added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './auth/login/login.component';
@@ -45,6 +46,7 @@ import { environment } from '../environments/environment';
     MaterialModule,
     ReactiveFormsModule,
     AngularFirestoreModule,
+    AngularFireAuthModule,
     AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [AuthService, TrainingService],
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -3,27 +3,41 @@ import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable()
 export class AuthService {
     private user: User;
     authChange = new Subject <boolean>();
-    constructor(private router: Router){}
+    constructor(private router: Router, private angularFireAuth: AngularFireAuth){}
 
     register (authData: AuthData) {
-        this.user = {
-            email: authData.email,
-            id: Math.round(Math.random() * 10000).toString()
-        };
+        this.angularFireAuth.auth.createUserWithEmailAndPassword(authData.email, authData.password).
+        then(auth => {
+            this.user = {
+                email: authData.email,
+                id: Math.round(Math.random() * 10000).toString()
+            };
+        })
+        .catch(err => {
+            console.error(authData);
+        });
         this.authSuccess();
     }
 
     login(authData: AuthData) {
-        this.user = {
-            email: authData.email,
-            id: Math.round(Math.random() * 10000).toString()
-        };
-        this.authSuccess();
+        
+        this.angularFireAuth.auth.signInWithEmailAndPassword(authData.email, authData.password)
+        .then(data => {
+            this.authSuccess();
+            this.user = {
+                email: authData.email,
+                id: Math.round(Math.random() * 10000).toString()
+            };
+        }).catch(err => {
+            console.log(err);
+        });
+        
     }
 
     logout() {
